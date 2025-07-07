9c20a081e9ca19c248b239c2e8f010b835d552bd
Fahmi Fauzi Rahman <fahmifauzirahman@gmail.com>
2025-07-07 20:34:06 +0700

    fix policy

--- app/Policies/ActivityPlanDetailPolicy.php
+++ app/Policies/ActivityPlanDetailPolicy.php
@@ -34,7 +34,7 @@ class ActivityPlanDetailPolicy
         if ($user->role === User::Role_Admin) return true;
 
         if ($user->role === User::Role_BS) {
-            return $item->user_id == $user->id;
+            return $item->id ? $item->user_id == $user->id : true;
         }
 
         if ($user->role === User::Role_Agronomist) {
